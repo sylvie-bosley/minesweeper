dcd86ac8d1bcf3ab13b34980799db557a9b6f48c
pierce-bosley <pierce.bosley@gmail.com>
2021-06-19 15:32:47 -0700

    Add #init, #reveal, and getters to Tile class

--- lib/tile.rb
+++ lib/tile.rb
@@ -1,7 +1,15 @@
 module Minesweeper
   class Tile
-    def initialize
+    attr_reader :hidden, :flagged
 
+    def initialize(bomb)
+      @revealed = false
+      @flagged = false
+      @bomb = bomb
+    end
+
+    def reveal!
+      @revealed = true
     end
   end
 end
