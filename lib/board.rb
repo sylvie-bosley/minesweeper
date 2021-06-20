368b386ffd0f81d0e5045905998a257bdf94a668
pierce-bosley <pierce.bosley@gmail.com>
2021-06-19 19:15:18 -0700

    Add board and mine initialization

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,44 @@
+require_relative "tile"
+
 module Minesweeper
   class Board
-    def initialize(tiles)
-      @grid = tiles
+    def initialize(dimensions, mines)
+      @grid = build_grid(mines, *dimensions)
     end
 
     def [](position)
       row, col = position
       @grid[row][col]
+    end
+
+    private
+
+    def build_grid(mines, rows, cols)
+      new_grid = Array.new(rows) { Array.new(cols) }
+      mine_positions = generate_mine_positions(rows, cols, mines)
+
+      rows.times do |row|
+        cols.times do |col|
+          if mine_positions.include?([row, col])
+            new_grid[row][col] = Tile.new(true)
+          else
+            new_grid[row][col] = Tile.new(false)
+          end
+        end
+      end
+
+      new_grid
+    end
+
+    def generate_mine_positions(rows, cols, mines)
+      positions = []
+
+      until positions.length == mines
+        new_position = [rand(rows), rand(cols)]
+        positions << new_position unless positions.include?(new_position)
+      end
+
+      positions
     end
   end
 end
