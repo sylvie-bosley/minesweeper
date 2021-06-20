9da4d585d6c61b5b9d39d254936b90f4fe8de1e3
pierce-bosley <pierce.bosley@gmail.com>
2021-06-19 17:05:59 -0700

    Define Tile#to_s

--- lib/tile.rb
+++ lib/tile.rb
@@ -1,6 +1,10 @@
 module Minesweeper
   class Tile
-    attr_reader :hidden, :flagged, :bomb
+    HIDDEN = "#"
+    EMPTY = " "
+    private_constant :HIDDEN
+
+    attr_reader :revealed, :flagged, :bomb
 
     def initialize(bomb)
       @adjacent_bombs = 0
@@ -12,6 +16,12 @@ module Minesweeper
     def reveal!(adjacent_bombs)
       @revealed = true
       @adjacent_bombs = adjacent_bombs
+    end
+
+    def to_s
+      return HIDDEN unless @revealed
+      return EMPTY if @adjacent_bombs.zero?
+      @adjacent_bombs.to_s
     end
   end
 end
