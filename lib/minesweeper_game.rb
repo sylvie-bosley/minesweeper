6d01037d9078731089c2106aa54378854c8332cc
pierce-bosley <pierce.bosley@gmail.com>
2021-06-20 17:10:55 -0700

    Add logic for prompting user for difficuly level

--- lib/minesweeper_game.rb
+++ lib/minesweeper_game.rb
@@ -1,7 +1,21 @@
 require_relative "board"
 
 module Minesweeper
+  DIFFICULTY_LEVELS = ["beginner", "intermediate", "expert"]
+
   class MineGame
+    def self.get_difficulty
+      difficulty = ""
+
+      until DIFFICULTY_LEVELS.include?(difficulty)
+        puts "Choose a difficulty level:"
+        puts "beginner, intermediate, or expert"
+        difficulty = gets.chomp.downcase
+      end
+
+      difficulty
+    end
+
     def initialize(difficulty)
 
     end
